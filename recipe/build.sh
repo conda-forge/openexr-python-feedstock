7b244af8c663881b233369e071eea5b1fd753394
Peter Williams <peter@newton.cx>
2022-03-08 22:04:10 -0500

    Update for OpenEXR 3.x series

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 
 set -xeuo pipefail
-export CPPFLAGS="$CPPFLAGS -I$PREFIX/include/OpenEXR"
+export CPPFLAGS="$CPPFLAGS -I$PREFIX/include/Imath -I$PREFIX/include/OpenEXR"
 exec $PYTHON -m pip install . -vv
