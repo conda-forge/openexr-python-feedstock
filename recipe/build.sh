e87a7c367d9a98870f88487ef6a53ac19f691f2b
Peter Williams <peter@newton.cx>
2024-11-17 11:06:02 -0500

    Switch to scikit-build build system
    This version drops `setup.py` and so we are force to migrate to the
    scikit-build build. This version of the build appears to build the main
    OpenEXR library statically and link it all into the `OpenEXR.$SHLIB_EXT`
    binary Python module, which means that we are *not* actually reusing the
    libOpenEXR provided by the conda-forge `openexr` package. But we are
    depending on conda-forge's libImath, so at least that's something.
    
    With this new build system, we should probably turn `openexr-feedstock`
    into a multi-output build and retire this feedstock. AFAICT the source
    file on pypi.org is identical to the source file used for the
    `openexr-feedstock` build.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,5 @@
 
 set -xeuo pipefail
 export CPPFLAGS="$CPPFLAGS -I$PREFIX/include/Imath -I$PREFIX/include/OpenEXR"
+export CMAKE_ARGS="$CMAKE_ARGS -DOPENEXR_FORCE_INTERNAL_DEFLATE=OFF -DOPENEXR_FORCE_INTERNAL_IMATH=OFF"
 exec $PYTHON -m pip install . -vv
